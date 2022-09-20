f29683324ca0c2653d7f13edb28aef30536c5eb8
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-20 14:49:28 -0500

    double checked imports

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5002;
 const feedback = require('./Routes/feedback.router.js')
 
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
+//const feedback = require('./Routes/feedback.router')
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/feedback', feedback);
